473240e2e00587fada9ae550b11a1b202040ce71
nicolabcraft <nicolabcraft.2@gmail.com>
2024-01-14 02:50:05 +0100

    feat: :sparkles: command reload

---
+++ commands/slash/General/reload.js
@@ -0,0 +1,33 @@
+const { EmbedBuilder, Collection, ActionRowBuilder, StringSelectMenuBuilder,ButtonBuilder, ButtonStyle, Events, PermissionsBitField } = require("discord.js");
+const config = require('../../../config/config.js');
+const mysql = require('mysql');
+
+//////////////////////////
+//         CODE        //
+////////////////////////
+
+module.exports = {
+    name: "reload",
+    description: "Reload Admin",
+    type: 1,
+    options: [],
+    permissions: {
+        DEFAULT_MEMBER_PERMISSIONS: "SendMessages"
+    },
+    run: async (client, interaction, config) => {
+            if(!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)){
+                return;
+            }else{
+                    const embeddrop = new EmbedBuilder()
+                    .setTitle(`Reload`)
+                    .setDescription(
+                        `Le bot a été redémarré avec succès.                        
+                         Date:  ${new Date().toLocaleString('fr-FR', { timeZone: 'Europe/Paris' })}`
+                    )
+                interaction.reply({ embeds: [embeddrop], components: [], ephemeral: true, });
+                setTimeout(() => {
+                    console.log(`[INFO] Bot redémarré avec succès ! Date: ${new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')}`)
+                }, 500);
+            }
+    },
+};
