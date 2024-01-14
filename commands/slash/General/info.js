b733cf171ee40a27f39d8fb1533b1aea8ca2a4c0
nicolabcraft <nicolabcraft.2@gmail.com>
2024-01-14 10:41:24 +0100

    fix: :bug: dbb connexion for verifsico

--- commands/slash/General/info.js
+++ commands/slash/General/info.js
@@ -1,4 +1,5 @@
 const { EmbedBuilder, Collection, ActionRowBuilder, ButtonBuilder, ButtonStyle, Events, PermissionsBitField } = require("discord.js");
+var connection = mysql.createConnection(config.Bdd);
 const mysql = require('mysql');
 module.exports = {
     name: "info", // Name of command
